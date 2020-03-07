3606ca220b0a405d4df6c991962ede90c2eca2bd
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-07 22:53:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -104,7 +104,7 @@ def checkout(cart, coupons)
   cart_with_coupons = apply_coupons(consolidated_cart, coupons)
   puts "CART_WITH_COUPONS"
   pp cart_with_coupons
-  cart_with_clearance = apply_clearance(cart)
+  cart_with_clearance = apply_clearance(cart_with_coupons)
   puts "CART_WITH_CLEARANCE"
   pp cart_with_clearance
   
